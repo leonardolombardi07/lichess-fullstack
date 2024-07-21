82c5819e6db9d3238313568ffb4e81ec3e423218
Leonardo Lombardi <leonardolombardi@poli.ufrj.br>
2024-07-21 20:13:28 -0300

    fix compiler errors

--- src/app/players/[playerUsername]/page.tsx
+++ src/app/players/[playerUsername]/page.tsx
@@ -1,9 +1,30 @@
-import LichessApi from "@/modules/lichess-api";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
-export default async function Page() {
-  const leaderboard = await LichessApi.getSingleLeaderboard({
-    numPlayers: 50,
-    perfType: "classical",
-  });
-  return 2;
+interface PageProps {
+  params: {
+    playerUsername: string;
+  };
+}
+
+export default async function Page({ params }: PageProps) {
+  return (
+    <Box
+      sx={{
+        p: 2,
+      }}
+    >
+      <WorkInProgress />
+    </Box>
+  );
+}
+
+function WorkInProgress() {
+  return (
+    <Alert severity="info">
+      <AlertTitle>Work in progress</AlertTitle>
+      This page is still under construction. Please check back later.
+    </Alert>
+  );
 }
